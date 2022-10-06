09a199f699efc3868a5cc97f341475a4aa98a0bd
moxAnustasiya <nassstasiya@gmail.com>
2022-10-06 22:09:26 +0300

    Создала класс для преобразования json в xml

--- src/main/java/JSON/jsontoxml.java
+++ src/main/java/JSON/jsontoxml.java
@@ -1,4 +1,31 @@
 package JSON;
 
-public class jsontoxml {
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.logging.Logger;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
+import static Read.ReadJSON.read;
+
+public class jsontoxml
+{
+    private static JSONObject object = new JSONObject();
+    private static Hospital hospital;
+    public jsontoxml(File file)
+    {
+        JSONParser parser = new JSONParser();
+        try
+        {
+            JSONObject object = (JSONObject) parser.parse(new FileReader(file.getName()));
+            this.object = object;
+        }
+        catch (IOException | ParseException ex) {Logger.getLogger(jsontoxml.class.getName());}
+    }
+    private static void Reading()
+    {
+        hospital = read(object);
+    }
 }

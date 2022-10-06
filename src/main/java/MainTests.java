d0899ce442cecf46cc503b953dffa7c69c20ed4c
moxAnustasiya <nassstasiya@gmail.com>
2022-10-06 19:46:51 +0300

    Добавила ввод имени файла

--- src/main/java/MainTests.java
+++ src/main/java/MainTests.java
@@ -1,13 +1,17 @@
 import XML.xmltojson;
 
 import java.io.File;
+import java.util.Scanner;
 
 public class MainTests
 {
     public static void main(String[] args)
     {
+        System.out.println("Введите имя файла с расширением xml: ");
+        Scanner in = new Scanner(System.in);
+        String name = in.nextLine();
         String fileName = "C:\\Users\\Антон\\Desktop\\ФИб5\\Практика\\Задание 2\\FileConverterService\\"
-                +"БольницаXML"+".xml";
+                +name+".xml";
         File file = new File(fileName);
         xmltojson parser = new xmltojson(file);
         parser.ParsingXMLtoJSON();
